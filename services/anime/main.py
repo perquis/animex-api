a2b720c6d8e9900f9fb01f9c952157b18ed9d6bb
PerQuis <thetahirpl@gmail.com>
2024-07-28 23:24:40 +0200

    :sparkles: feature(main.py): introduced redis-client to cache data.

--- services/anime/main.py
+++ services/anime/main.py
@@ -1,8 +1,10 @@
+import json
 from typing import Annotated
 
 from controllers.AnimeController import get_anime_object
 from fastapi import FastAPI, Path
 from models import AnimeModel
+from services.redis import redis_client
 from utils.process_data import process_data
 
 app = FastAPI(swagger_ui_parameters={
@@ -16,10 +18,15 @@ app = FastAPI(swagger_ui_parameters={
 def get_anime_info(
     anime_id: Annotated[int, Path(title="Anime ID", description="The `ID` of the anime from the **myanimelist**.", ge=1, le=100_000)]
 ) -> AnimeModel:
-    url = f"https://myanimelist.net/anime/{anime_id}"
-    anime_instance = get_anime_object(url)
+    if redis_client.exists(f"get_anime_info:{anime_id}"):
+        return json.loads(redis_client.get(f"get_anime_info:{anime_id}"))
+    else:
+        url = f"https://myanimelist.net/anime/{anime_id}"
+        anime_instance = get_anime_object(url)
 
-    process_data(anime_instance['production'])
-    process_data(anime_instance['demographics'])
-    
-    return anime_instance
+        process_data(anime_instance['production'])
+        process_data(anime_instance['demographics'])
+
+        redis_client.set(f"get_anime_info:{anime_id}", json.dumps(anime_instance), ex=3600)
+        
+        return anime_instance
