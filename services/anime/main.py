ff2fdd1e594a39e752d267bbf067f5d8b2816dea
PerQuis <thetahirpl@gmail.com>
2024-07-29 11:56:27 +0200

    :lock: security(CORS): introduced cors settings in main.py.

--- services/anime/main.py
+++ services/anime/main.py
@@ -1,5 +1,6 @@
 import redis.asyncio as redis
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_limiter import FastAPILimiter
 from routes.AnimeRouter import anime_router
 
@@ -13,5 +14,13 @@ app = FastAPI(swagger_ui_parameters={
 async def startup():
     redis_connection = redis.from_url("redis://localhost", encoding="utf-8", decode_responses=True)
     await FastAPILimiter.init(redis_connection)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(anime_router, prefix="/api/v1")
